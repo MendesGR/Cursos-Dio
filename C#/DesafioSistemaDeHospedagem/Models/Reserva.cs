842d8d85d8a1b8f3734ecc9f8d1530f4fd72c9d5
Gabriel Mendes <102915068+MendesGR@users.noreply.github.com>
2025-09-22 16:48:30 -0300

    Add files via upload

---
+++ C#/DesafioSistemaDeHospedagem/Models/Reserva.cs
@@ -0,0 +1,57 @@
+using DesafioSistemaDeHospedagem.Models;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verifica se a capacidade da suíte é suficiente para os hóspedes
+            if (Suite != null && hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // Lança uma exceção se a capacidade for menor
+                throw new ArgumentException("A quantidade de hóspedes excede a capacidade da suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes
+            return Hospedes != null ? Hospedes.Count : 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Se reservar por 10 dias ou mais, aplicar 10% de desconto
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.10m; // ou: valor *= 0.90m;
+            }
+
+            return valor;
+        }
+    }
+}

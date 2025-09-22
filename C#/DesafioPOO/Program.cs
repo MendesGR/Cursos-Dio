0e0d1bcab5bb0b75b05fb5b8e6f616c35f7361f2
Gabriel Mendes <102915068+MendesGR@users.noreply.github.com>
2025-09-22 14:56:41 -0300

    Add files via upload

---
+++ C#/DesafioPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration.Assemblies;
+using DesafioPOO.Models;
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("9999-0000", "Nokia Tijolão", "123456789", 8);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("8888-0000", "iPhone 13", "987654321", 512);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }   
+}
